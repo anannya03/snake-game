98537c9076969e1a2824df1420cbe0ae8f7c42b2
Anannya Patra <aaniee09@gmail.com>
2020-07-04 20:56:12 +0530

    Modified files

--- Main.java
+++ Main.java
@@ -4,14 +4,20 @@ import javax.swing.JFrame;
 
 public class Main{
     public static void main(String[] args) {
-        JFrame obj = new JFrame();
+        //Create the JFrame
+        JFrame myFrame = new JFrame();
+        //Create object of the Game class
         Game myGame = new Game();
-        obj.setBounds(10, 10, 900, 700);
-        obj.setBackground(Color.DARK_GRAY);
-        obj.setResizable(false);
-        obj.setVisible(true);
-        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        obj.add(myGame);
+        //The dimensions of the frame
+        myFrame.setBounds(10, 10, 900, 700);
+        //The background color of the JFrame
+        myFrame.setBackground(Color.DARK_GRAY);
+        //The frame is set to be not resizable
+        myFrame.setResizable(false);
+        myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Add the class to the frame object
+        myFrame.add(myGame);
         
     }
 }
